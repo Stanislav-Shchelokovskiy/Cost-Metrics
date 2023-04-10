0544286d8546a81063f1518d9c4fa3cce4e96dbb
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-04-10 16:57:55 +0400

    initial

---
+++ .dev_env.sh
@@ -0,0 +1,8 @@
+importenv
+export REDIS_SERVICE=localhost
+export CELERY_BROKER_URL=redis://${REDIS_SERVICE}:6380/0
+export CELERY_RESULT_BACKEND=redis://${REDIS_SERVICE}:6380/0
+export DB_HOME=/home/shchelokovskiy/code/support_metrics/data
+export SQLITE_DATABASE=/home/shchelokovskiy/code/support_metrics/data/db
+export REDIS_DB=SUPPORT_METRICS
+export QUERY_SERVICE=localhost:11005
