705e795b08d846744c5c5f310be2b305e5a476df
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-05-20 13:30:18 +0400

    use save_table_operation

--- tasks/cost_metrics_tasks.py
+++ tasks/cost_metrics_tasks.py
@@ -1,7 +1,4 @@
-import os
-import json
-from pandas import DataFrame
-import toolbox.sql.sqlite_db as sqlite_db
+from toolbox.sql.connections import SqliteConnection
 from toolbox.sql.crud_queries.protocols import CRUDQuery
 from toolbox.sql.crud_queries.upsert import SqliteUpsertQuery
 from sql_queries.transform_load.table_defs import get_create_table_statements
@@ -9,30 +6,28 @@ from sql_queries.transform_load.index_defs import get_create_index_statements
 from repository import RemoteRepository
 from sql_queries.index import local_names_index
 from sql_queries.meta import CostmetricsMeta
+from toolbox.sql.db_operations import SaveTableOperation
 
 
-# yapf: disable
-def _save_tables(tables: dict[str, DataFrame | CRUDQuery]):
-    sqlitedb = sqlite_db.get_or_create_db()
-    sqlitedb.save_tables(
-        tables=tables,
-        tables_defs=get_create_table_statements(),
-        create_index_statements=get_create_index_statements(),
-    )
-# yapf: enable
+def _save_tables(*queries: CRUDQuery):
+    [
+        SaveTableOperation(
+            conn=SqliteConnection(),
+            query=query,
+            tables_defs=get_create_table_statements(),
+            create_index_statements=get_create_index_statements(),
+        )() for query in queries
+    ]
 
 
 def update_cost_metrics(kwargs: dict):
     df = RemoteRepository.cost_metrics.get_data(**kwargs)
     _save_tables(
-        tables={
-            local_names_index.get_cost_metrics_table_name():
-                SqliteUpsertQuery(
-                    table_name=local_names_index.get_cost_metrics_table_name(),
-                    cols=df.columns,
-                    key_cols=CostmetricsMeta.get_key_fields(),
-                    confilcting_cols=CostmetricsMeta.get_conflicting_fields(),
-                    rows=df.itertuples(index=False),
-                )
-        }
+        SqliteUpsertQuery(
+            table_name=local_names_index.get_cost_metrics_table_name(),
+            cols=df.columns,
+            key_cols=CostmetricsMeta.get_key_fields(),
+            confilcting_cols=CostmetricsMeta.get_conflicting_fields(),
+            rows=df.itertuples(index=False),
+        )
     )
