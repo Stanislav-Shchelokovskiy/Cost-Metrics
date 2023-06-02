978b07743726bc1e6ca3da83aa73c5fa39fb2350
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-06-02 09:41:27 +0400

    extract period from data

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,4 @@
 from config.config import (
     get_cost_metrics_period,
-    get_cost_metrics_period_json,
     get_work_on_holidays_period,
 )
-
-
--- repository/metrics/local/cost_metrics/repository.py
+++ repository/metrics/local/cost_metrics/repository.py
@@ -11,6 +11,7 @@ from repository.metrics.local.cost_metrics.tribes import TribesQueryDescriptor
 from repository.metrics.local.cost_metrics.positions import PositionsQueryDescriptor
 from repository.metrics.local.cost_metrics.employees import EmployeesQueryDescriptor
 from repository.metrics.local.cost_metrics.teams import TeamsQueryDescriptor
+from repository.metrics.local.cost_metrics.period import PeriodQueryDescriptor
 from repository.metrics.local.generators import get_groupbys
 from toolbox.utils.converters import Object_to_JSON
 
@@ -26,6 +27,7 @@ class CostMetricsRepository:
         self.positions = create_repository(PositionsQueryDescriptor())
         self.employees = create_repository(EmployeesQueryDescriptor())
         self.teams = create_repository(TeamsQueryDescriptor())
+        self.period = create_repository(PeriodQueryDescriptor())
 
     async def get_metrics(self) -> str:
         return Object_to_JSON.convert(get_metrics())
