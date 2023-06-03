039a5f168f3020516a7fc4cb67629b0794192a1a
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-06-03 18:48:56 +0400

    use generics for swagger

--- server_models.py
+++ server_models.py
@@ -6,7 +6,7 @@ from toolbox.server_models import (
 
 
 class CostMetricsParams(ServerModel):
-    teams: FilterParametersNode | None = Field(alias='Teams')
-    tribes: FilterParametersNode | None = Field(alias='Tribes')
-    positions: FilterParametersNode | None = Field(alias='Positions')
-    employees: FilterParametersNode | None = Field(alias='Employees')
+    teams: FilterParametersNode[int] | None = Field(alias='Teams')
+    tribes: FilterParametersNode[str] | None = Field(alias='Tribes')
+    positions: FilterParametersNode[str] | None = Field(alias='Positions')
+    employees: FilterParametersNode[str] | None = Field(alias='Employees')
