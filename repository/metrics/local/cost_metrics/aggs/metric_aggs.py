1a799e10da79210de17f409c28f57af7b0dd2823
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-06-09 14:45:31 +0400

    extract sql funcs and metric

--- repository/metrics/local/cost_metrics/aggs/metric_aggs.py
+++ repository/metrics/local/cost_metrics/aggs/metric_aggs.py
@@ -1,94 +1,8 @@
 import os
-from typing import NamedTuple
 from collections.abc import Iterable, Callable, Mapping
 from collections import ChainMap
 from sql_queries.meta.cost_metrics import CostmetricsMeta
-
-
-class Func:
-
-    def _as_str(self, window: str = '') -> str:
-        pass
-
-    def __str__(self) -> str:
-        return self._as_str()
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def __add__(self, other: 'Func') -> 'Func':
-        return SUM('', self, other, op=' + ')
-
-    def __mul__(self, other: 'Func') -> 'Func':
-        return SUM('', self, other, op=' * ')
-
-    def __truediv__(self, other: 'Func') -> 'Func':
-        return DIV(self, other)
-
-    def __eq__(self, other: 'Func') -> bool:
-        return str(self) == str(other)
-
-    def over(self, window: str) -> str:
-        return self._as_str(window)
-
-
-class DIV(Func):
-
-    def __init__(self, dividee: Func, divider: Func) -> None:
-        self.dividee = dividee
-        self.divider = divider
-
-    def _as_str(self, window: str = ''):
-        # yapf: disable
-        dividee, divider = self.dividee, self.divider
-        if window:
-            dividee, divider = self.dividee.over(window), self.divider.over(window)
-        return f'IIF({divider} = 0, 0, {dividee} * 1.0 / {divider})'
-        # yapf: enable
-
-
-class SUM(Func):
-
-    def __init__(self, param: str, *expressions: Func, op: str = '') -> None:
-        self.expressions = expressions or [f'SUM({param})']
-        self.op = op
-
-    def _as_str(self, format: Callable[[Func], str] = lambda x: str(x)):
-        res = self.op.join(format(expr) for expr in self.expressions)
-        if len(self.expressions) > 1 and (' + ' == self.op):
-            return f'({res})'
-        return res
-
-    def over(self, window: str) -> str:
-        return self._as_str(lambda x: f'{x} OVER ({window})')
-
-
-class Metric(NamedTuple):
-    name: str
-    expression: Func
-
-    def __str__(self) -> str:
-        return str(self.expression)
-
-    def __add__(self, other: 'Metric'):
-        return Metric('', self.expression + other.expression)
-
-    def __mul__(self, other: 'Metric'):
-        return Metric('', self.expression * other.expression)
-
-    def __truediv__(self, other: 'Metric'):
-        return Metric('', self.expression / other.expression)
-
-    def __eq__(self, other: 'Metric') -> bool:
-        return str(self) == str(other)
-
-    @classmethod
-    def from_metric(cls, name: str, metric: 'Metric'):
-        return cls(name, metric.expression)
-
-    def get_over(self, window: str) -> str:
-        return self.expression.over(window)
-
+from toolbox.sql.aggs import Metric, SUM
 
 # yapf: disable
 
@@ -186,7 +100,7 @@ def get_metric(metric: str, mode: str | None) -> Metric:
     return get_metrics(mode).get(metric, none_metric)
 
 
-def get_metrics_names(mode: str | None, formatter: Callable[[Metric], str]= lambda x: x.name) -> Iterable:
+def get_metrics_names(mode: str | None, formatter: Callable[[Metric], str] = lambda x: x.name) -> Iterable:
     return [formatter(x) for x in get_metrics(mode).values()]
 
 
