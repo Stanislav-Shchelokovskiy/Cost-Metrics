0ef8c08ca4803c6f766be8289ae019c0a14bdf39
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-06 19:29:17 +0400

    add dev port

--- .dev_env.sh
+++ .dev_env.sh
@@ -7,3 +7,4 @@ export SQLITE_DATABASE=/home/shchelokovskiy/code/cost_metrics/data/db
 export REDIS_DB=COST_METRICS
 export QUERY_SERVICE=localhost:11005
 export RECALCULATE_FROM_THE_BEGINNING=0
+export SERVER_PORT=11002
