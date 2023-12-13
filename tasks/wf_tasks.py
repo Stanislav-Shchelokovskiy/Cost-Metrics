51045aa99fcc64e86319ef1c9fdb01581a227973
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-12-13 10:28:09 +0400

    refactor

--- tasks/wf_tasks.py
+++ tasks/wf_tasks.py
@@ -3,29 +3,19 @@ import toolbox.utils.network as network
 from repository import WfRepository
 
 
-def _get_wf_response(
-    method: str,
-    params: dict[str, str],
-) -> str:
-    return network.get_data(
-        end_point=os.environ['WF_ENDPOINT'] + method,
-        headers={
-            os.environ['WF_LOGIN_HEADER']: os.environ['WF_LOGIN'],
-            os.environ['WF_LOGIN_PASSWORD']: os.environ['WF_PASSWORD'],
-            'User-Agent': 'CostMetrics',
-        },
-        params=params,
-    ).replace('":', '": ')
-
-
 def _get_wf_hours(start: str, end: str) -> str:
-    return _get_wf_response(
-        method='GetDayAppointmentAnalysis',
-        params={
-            'start': start,
-            'end': end,
+    return network.get_data(
+        end_point=os.environ["WF_ENDPOINT"],
+        headers={
+            os.environ["WF_LOGIN_HEADER"]: os.environ["WF_LOGIN"],
+            os.environ["WF_LOGIN_PASSWORD"]: os.environ["WF_PASSWORD"],
+            "User-Agent": "CostMetrics",
         },
-    )
+        params={
+            "start": start,
+            "end": end,
+        },
+    ).replace('":', '": ')
 
 
 def upsert_work_hours(start: str, end: str):
