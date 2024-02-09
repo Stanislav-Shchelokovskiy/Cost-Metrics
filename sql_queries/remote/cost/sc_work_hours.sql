3e754badfbf881fbd4b403e1a8bd19cca5d9e4bd
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-02-09 14:35:57 +0400

    stricter work hours

--- sql_queries/remote/cost/sc_work_hours.sql
+++ sql_queries/remote/cost/sc_work_hours.sql
@@ -1,8 +1,8 @@
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 
-DECLARE @employees VARCHAR(MAX) = N'{employees_json}'
-DECLARE @start	DATE = '{start}'
-DECLARE @end	DATE = '{end}'
+DECLARE @employees 	VARCHAR(MAX) = N'{employees_json}'
+DECLARE @start		DATE = '{start}'
+DECLARE @end		DATE = '{end}'
 
 DROP TABLE IF EXISTS #SCWorkHours;
 
@@ -10,7 +10,7 @@ SELECT	emp_scid									AS emp_scid,
 		DATEFROMPARTS(YEAR(date), MONTH(date), 1)	AS year_month,
 		SUM(work_hours)							    AS work_hours
 INTO	#SCWorkHours
-FROM	DXStatisticsV2.dbo.sc_work_hours(@start, @end, @employees)
+FROM	DXStatisticsV2.dbo.sc_work_hoursTMP(@start, @end, @employees)
 /*	Don't group by anything else here. Otherwise make sure to filter result further by the new group field. */
 GROUP BY	emp_scid,
 			DATEFROMPARTS(YEAR(date), MONTH(date), 1)
